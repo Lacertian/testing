d89d93b72c7a0dd912cce9d81152f1bfe8f9ef13
Lacertian <40523324+Lacertian@users.noreply.github.com>
2018-09-06 21:19:56 +0300

    Update language.js

--- language.js
+++ language.js
@@ -3,22 +3,21 @@ function handler(e){
     let lang = e.target.dataset.lang,
         view = document.querySelector('#result');
     
-    // Внутри ставим куки и переход
+ 
     switch(lang){
       case 'ua':
-        //view.textContent = 'Выбран русский язык';
             window.location.href = "Interwork-ua.html";
       break;
+      
       case 'en':
-        //view.textContent = 'English is the selected language';
             window.location.href = "Interwork-en.html";
       break;
+     
       case 'de':
-        //view.textContent = 'Ausgewählte Deutsch';
             window.location.href = "Interwork-de.html";
       break;
-            case 'ru':
-        //view.textContent = 'Ausgewählte Deutsch';
+      
+      case 'ru':
             window.location.href = "Interwork-ru.html";
       break;
     }
